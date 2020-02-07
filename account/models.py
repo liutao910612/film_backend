0620a12560f014622e8ba5f1cd44c9f4c2d2e937
DESKTOP-FEURCKB\yyt_c <liutao910612@126.com>
2020-02-07 21:11:32 +0800

    improve model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class BasicModel(models.Model):
+    add_time = models.DateTimeField(auto_now_add=True, null=True)
+    add_user = models.IntegerField(null=True)
+    update_time = models.DateTimeField(auto_now=True)
+    update_user = models.IntegerField(null=True)
+    is_deleted = models.IntegerField(choices=('0', '1'), null=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(BasicModel):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+    email = models.EmailField
+    phone = models.CharField(max_length=50)
+    is_admin = models.IntegerField(choices=('0', '1'))
+
+    @property
+    def username(self):
+        return '%s %s'.format(self.first_name, self.last_name)
+
+
+class UserDetail(User):
+    user_id = models.IntegerField(null=False)
+    address = models.CharField(max_length=200)
+    hobby = models.CharField(max_length=200)
--- movie/models.py
+++ movie/models.py
@@ -2,24 +2,31 @@ from django.db import models
 
 
 class BasicModel(models.Model):
+    add_time = models.DateTimeField(auto_now_add=True, null=True)
+    add_user = models.IntegerField(null=True)
+    update_time = models.DateTimeField(auto_now=True)
+    update_user = models.IntegerField(null=True)
+    is_deleted = models.IntegerField(choices=('0', '1'), null=True)
 
-    pass
+    class Meta:
+        abstract = True
 
 
-# Create your models here.
-class Film(models.Model):
-    id=models.AutoField(primary_key=True)
+class Film(BasicModel):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    img = models.CharField(max_length=200)
-    video = models.CharField(max_length=200)
+    img = models.CharField(max_length=200,null=True)
+    video = models.CharField(max_length=200,null=True)
     suppose_num = models.IntegerField(default=0)
     download_num = models.IntegerField(default=0)
-    main_page = models.CharField(max_length=200)
-    add_time = models.DateTimeField(auto_now_add=True)
-    add_user = models.IntegerField
-    update_time = models.DateTimeField(auto_now=True)
-    update_user = models.IntegerField
+    main_page = models.CharField(max_length=200,null=True)
 
     def __str__(self):
         return self.name
 
+
+class Comment(BasicModel):
+    id = models.AutoField(primary_key=True)
+    movie_id = models.IntegerField
+    context = models.TextField
+    check = models.IntegerField(choices=('0', '1'))
