082318c0c29878f536eba75c51e224bcdd8e408d
DESKTOP-FEURCKB\yyt_c <liutao910612@126.com>
2020-02-12 21:30:58 +0800

    init project

--- movie/urls.py
+++ movie/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path, re_path
 
 from movie import views
+from movie.views import FilmView
 
 urlpatterns = [
-    path('support/<int:user_id>', views.support_film),
+
+    # you can user reverse() to get url ,for example: reverse('support-film', args=(112,))
+    path('support', views.support_film, name='support-film'),
     re_path(r'^list/(?P<page>[1-9]\d*)/(?P<amount>[1-9]\d*)/$', views.get_films),
+    path('',FilmView.as_view())
 
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,19 +1,40 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.views.decorators.http import require_http_methods, require_POST
 
 
-def support_film():
+@require_http_methods(["POST"])  # require_POST()
+def support_film(request):
     """
     Give a like to film
     :return:
     """
-    pass
+
+    # Any view should return httpResponse object
+    return HttpResponse('ok')
 
 
-def get_films(start=1, amount=5):
+@require_POST
+def get_films(request, start=1, amount=5):
     """
     Get the films
+    :param request:
     :param start:
     :param amount:
     :return:
     """
     pass
+
+
+class FilmView(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
