f4ba9fe955b639d0850496e560577aa4f5909e04
DESKTOP-FEURCKB\yyt_c <liutao910612@126.com>
2020-02-27 22:03:33 +0800

    Add the logger

--- film_backend/settings.py
+++ film_backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import time
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -127,5 +129,75 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
         'LOCATION': 'unique-snowflake',
         'KEY_PREFIX':'film_dev'
+    }
+}
+
+cur_path = os.path.dirname(os.path.realpath(__file__))
+log_path = os.path.join(os.path.dirname(cur_path), 'logs')
+if not os.path.exists(log_path): os.mkdir(log_path)
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': '[%(asctime)s] [%(filename)s:%(lineno)d] [%(module)s:%(funcName)s] '
+                      '[%(levelname)s]- %(message)s'},
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'filters': {
+    },
+    'handlers': {
+        'default': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(log_path, 'all-{}.log'.format(time.strftime('%Y-%m-%d'))),
+            'maxBytes': 1024 * 1024 * 5,  # 文件大小
+            'backupCount': 5,  # 备份数
+            'formatter': 'standard',  # 输出格式
+            'encoding': 'utf-8',  # 设置默认编码，否则打印出来汉字乱码
+        },
+        # 输出错误日志
+        'error': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(log_path, 'error-{}.log'.format(time.strftime('%Y-%m-%d'))),
+            'maxBytes': 1024 * 1024 * 5,  # 文件大小
+            'backupCount': 5,  # 备份数
+            'formatter': 'standard',  # 输出格式
+            'encoding': 'utf-8',  # 设置默认编码
+        },
+        # 控制台输出
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+        # 输出info日志
+        'info': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(log_path, 'info-{}.log'.format(time.strftime('%Y-%m-%d'))),
+            'maxBytes': 1024 * 1024 * 5,
+            'backupCount': 5,
+            'formatter': 'standard',
+            'encoding': 'utf-8',  # 设置默认编码
+        },
+    },
+    # 配置用哪几种 handlers 来处理日志
+    'loggers': {
+        # 类型为 django 处理所有类型的日志， 默认调用
+        'django': {
+            'handlers': ['error', 'console'],
+            'level': 'INFO',
+            'propagate': False
+        },
+        # log 调用时需要当作参数传入
+        'log': {
+            'handlers': ['error', 'console'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
     }
 }
--- movie/views.py
+++ movie/views.py
@@ -1,14 +1,16 @@
 import json
+import logging
 
 from django.forms import model_to_dict
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods, require_POST
 
+from basic.commonUtils import LocalCacheUtil
 from basic.httpUtils import ResponseHelper
 from movie import constants
 from movie.services import FilmService
-
+logger = logging.getLogger('log')
 
 @require_http_methods(["POST"])  # require_POST()
 def support_film(request):
@@ -16,7 +18,7 @@ def support_film(request):
     Give a like to film
     :return:
     """
-    film_id = request.GET.get('film_id')
+    film_id = request.POST.get('film_id')
     if film_id is None :
         return ResponseHelper.build_fail(constants.FILM_ID_IS_EMPTY)
 
@@ -38,8 +40,10 @@ def get_films(request, start=1, amount=5):
 
 
 class FilmView(View):
+
     def get(self, request):
         film_id = request.GET.get('film_id')
+        logger.debug("film_id:{}".format(film_id))
         if film_id is None:
             return ResponseHelper.build_fail("film_id is empty")
         film_service = FilmService()
@@ -79,4 +83,18 @@ class FilmView(View):
 
 
     def delete(self, request):
-        pass
+        # Check whether the user login
+        token = request.META.get("TOKEN")
+        if token is None:
+            return ResponseHelper.build_fail('Please login first')
+        user_dict = LocalCacheUtil.get_data("token-" + token.__str__())
+        if user_dict is not None:
+            return ResponseHelper.build_fail('Please login first')
+        film_id = request.GET.get("film_id")
+        if film_id is None :
+            return ResponseHelper.build_fail('Please choose the film to delete')
+        logger.debug('begin to delete film , film id is :{}'.format(film_id))
+
+        film_service = FilmService();
+        film_service.delete_movie(film_id)
+        return ResponseHelper.build_success()
